c2d676d5132b7272cb62158627b192febdacd9f3
zacglenn <emailzacglenn@cox.net>
2015-10-30 15:12:28 -0500

    Update Higher Than 75 Marks.sql

--- Higher Than 75 Marks.sql
+++ Higher Than 75 Marks.sql
@@ -2,7 +2,4 @@
 Enter your query here.
 Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
 */
-select Name
-from Students
-where Marks > 75
-order by right(Name, 3), ID
+select Name from Students where Marks > 75 order by right(Name, 3), ID;
