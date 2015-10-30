834848b80200c97d76936ccd773febb8f7407c79
zacglenn <emailzacglenn@cox.net>
2015-10-30 15:11:37 -0500

    Update Revising the Select Query - 2.sql

--- Revising the Select Query - 2.sql
+++ Revising the Select Query - 2.sql
@@ -2,7 +2,4 @@
 Enter your query here.
 Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
 */
-select Name
-from City
-where CountryCode = 'USA'
-    and Population > 120000
+select Name from City where CountryCode = 'USA' and Population > 120000;
